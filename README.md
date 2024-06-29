0fc57e8dd0f8d68b22ffddc4628ff32b7a1b642c
Artur Dobrowolski <artur.dobrowolski@startmail.com>
2024-06-29 19:03:41 +0200

    Changed group to nobody, dimmed lockscreen colors

---
+++ README.md
@@ -0,0 +1,11 @@
+# [slock-1.5](https://tools.suckless.org/slock/)
+Changed group to drop privileges to `nobody`, so it can work on a vanilla Arch Linux installation.
+
+## Installation
+To install slock simply run:
+```
+# make install clean
+```
+
+## Running slock
+Simply invoke the `slock` command. To get out of it, enter your password.
--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INPUT] =  "#115511",   /* during input */
+	[FAILED] = "#551111",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
